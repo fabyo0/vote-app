4b73fc0a44396d8a77e8d2a1ff64006a8ad76038
Emre Dikmen <emredikmen002@gmail.com>
2023-10-10 01:14:14 +0300

    modal toggle and transitions

--- resources/views/idea/show.blade.php
+++ resources/views/idea/show.blade.php
@@ -62,12 +62,15 @@
                                         @click.away="isOpen = false"
                                         @keydown.escape.window="isOpen = false"
                                     >
+
                                         <li><a href="#"
                                                class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark
-                                                as Spam</a></li>
+                                                as Spam</a>
+                                        </li>
                                         <li><a href="#"
                                                class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete
-                                                Post</a></li>
+                                                Post</a>
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
--- resources/views/livewire/idea-show.blade.php
+++ resources/views/livewire/idea-show.blade.php
@@ -2,14 +2,14 @@
 
     <div class="idea-container bg-white rounded-xl flex mt-4">
         <div class="flex flex-col md:flex-row flex-1 px-4 py-6">
-            <div class="flex-none mx-2 md:mx-4">
+            <div class="flex-none mx-2">
                 <a href="#">
                     <img src="{{ $idea->user->getAvatar() }}" alt="avatar" class="w-14 h-14 rounded-xl">
                 </a>
             </div>
             <div class="w-full mx-2 md:mx-4">
-                <h4 class="text-xl font-semibold">
-                    <a href="#" class="hover:underline">{{ $idea->title }}</a>
+                <h4 class="text-xl font-semibold mt-2 md:mt-0">
+                    {{ $idea->title }}
                 </h4>
                 <div class="text-gray-600 mt-3">
                     {{ $idea->description }}
@@ -31,15 +31,17 @@
                     >
                         <div
                             class="{{ $idea->status->classes }} text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">{{ $idea->status->name }}</div>
-                        <button
-                            class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
-                            @click="isOpen = !isOpen"
-                        >
-                            <svg fill="currentColor" width="24" height="6">
-                                <path
-                                    d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
-                                    style="color: rgba(163, 163, 163, .5)">
-                            </svg>
+                        <div class="relative">
+                            <button
+                                class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3"
+                                @click="isOpen = !isOpen"
+                            >
+                                <svg fill="currentColor" width="24" height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"
+                                        style="color: rgba(163, 163, 163, .5)">
+                                </svg>
+                            </button>
                             <ul
                                 class="absolute w-44 text-left font-semibold bg-white shadow-dialog rounded-xl z-10 py-3 md:ml-8 top-8 md:top-6 right-0 md:left-0"
                                 x-cloak
@@ -47,14 +49,26 @@
                                 @click.away="isOpen = false"
                                 @keydown.escape.window="isOpen = false"
                             >
+                                <li>
+                                    <a
+                                        href="#"
+                                        @click="
+                                                isOpen = false
+                                                $dispatch('custom-show-edit-modal')
+                                            "
+                                        class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3"
+                                    >
+                                        Edit Idea
+                                    </a>
+                                </li>
+                                <li><a href="#"
+                                       class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete
+                                        Idea</a></li>
                                 <li><a href="#"
                                        class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Mark as
                                         Spam</a></li>
-                                <li><a href="#"
-                                       class="hover:bg-gray-100 block transition duration-150 ease-in px-5 py-3">Delete
-                                        Post</a></li>
                             </ul>
-                        </button>
+                        </div>
                     </div>
 
                     <div class="flex items-center md:hidden mt-4 md:mt-0">
@@ -63,11 +77,21 @@
                                 class="text-sm font-bold leading-none @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                             <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
                         </div>
-                        <button
-                            class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
-                        >
-                            Vote
-                        </button>
+                        @if ($hasVoted)
+                            <button
+                                wire:click.prevent="vote"
+                                class="w-20 bg-blue text-white border border-blue font-bold text-xxs uppercase rounded-xl hover:bg-blue-hover transition duration-150 ease-in px-4 py-3 -mx-5"
+                            >
+                                Voted
+                            </button>
+                        @else
+                            <button
+                                wire:click.prevent="vote"
+                                class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5"
+                            >
+                                Vote
+                            </button>
+                        @endif
                     </div>
                 </div>
             </div>
@@ -125,44 +149,34 @@
                 </div>
             </div>
             @auth
-                @if( auth()->user()->isAdmin())
-                    <!-- Status Component -->
-                    <livewire:set-status
-                        :idea="$idea"
-                    />
+                @if (auth()->user()->isAdmin())
+                    <livewire:set-status :idea="$idea"/>
                 @endif
             @endauth
-
         </div>
 
         <div class="hidden md:flex items-center space-x-3">
             <div class="bg-white font-semibold text-center rounded-xl px-3 py-2">
-                <div class="text-xl leading-snug @if($hasVoted) text-blue @endif"
-                >{{ $votesCount }}</div>
+                <div class="text-xl leading-snug @if($hasVoted) text-blue @endif">{{ $votesCount }}</div>
                 <div class="text-gray-400 text-xs leading-none">Votes</div>
             </div>
-
-            @if($hasVoted)
+            @if ($hasVoted)
                 <button
-                    wire:click.prevent="vote"
                     type="button"
-                    class="flex items-center justify-center h-11 w-32 text-sm bg-blue text-white
-                font-semibold rounded-xl border border-blue hover:bg-blue-hover transition duration-150
-                ease-in px-6 py-3"
+                    wire:click.prevent="vote"
+                    class="w-32 h-11 text-xs bg-blue text-white font-semibold uppercase rounded-xl border border-blue hover:bg-blue-hover transition duration-150 ease-in px-6 py-3"
                 >
                     <span>Voted</span>
                 </button>
             @else
                 <button
-                    wire:click.prevent="vote"
                     type="button"
+                    wire:click.prevent="vote"
                     class="w-32 h-11 text-xs bg-gray-200 font-semibold uppercase rounded-xl border border-gray-200 hover:border-gray-400 transition duration-150 ease-in px-6 py-3"
                 >
                     <span>Vote</span>
                 </button>
             @endif
-
-
         </div>
     </div> <!-- end buttons-container -->
 </div>
