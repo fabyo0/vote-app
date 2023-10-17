7eb80e2d358ac85b58a279365902631561644158
Emre Dikmen <emredikmen002@gmail.com>
2023-10-18 00:58:53 +0300

    notification badge count

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -3,21 +3,47 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use function Symfony\Component\Translation\t;
 
 class CommentNotifications extends Component
 {
+
     public $notifications;
+    public $notificationCount;
+
+    public $isLoading;
+
+    public const NOTIFICATION_THRESHOLD = 20;
 
     protected $listeners = ['getNotifications'];
 
-    public function mount()
+
+    public function mount(): void
     {
         $this->notifications = collect([]);
+        $this->getNotificationCount();
+
+        $this->isLoading = true;
     }
 
-    public function getNotifications()
+    public function getNotificationCount(): void
     {
-        $this->notifications = auth()->user()->unreadNotifications()->get();
+        sleep(5);
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if ($this->notificationCount > self::NOTIFICATION_THRESHOLD) {
+            $this->notificationCount = self::NOTIFICATION_THRESHOLD . '+';
+        }
+    }
+
+    public function getNotifications(): void
+    {
+        $this->notifications = auth()->user()->unreadNotifications()
+            ->take(self::NOTIFICATION_THRESHOLD)
+            ->latest()
+            ->get();
+
+        $this->isLoading = false;
     }
 
     public function render()
