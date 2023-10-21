7daf9a3435c9b0e311538f3675b7242908d606da
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2023-10-21 18:24:36 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/user', function (\App\Models\User $user) {
-    return response()->json([
-       'user' => \App\Models\User::with('ideas.comments')->get()
-    ]);
-});
-
 Route::get('/clear', function () {
 
     Artisan::call('cache:clear');
