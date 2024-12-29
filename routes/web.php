25ad294f8d958a54be79d2b4cf61e79ca686a0d3
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2024-12-30 01:02:39 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IdeaController::class, 'index'])->name('idea.index');
 
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('idea.show');
+
 require __DIR__ . '/auth.php';
