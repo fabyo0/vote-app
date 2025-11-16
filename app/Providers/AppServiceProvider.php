582df44da5a8a92f08827c5501e0075b82fffb40
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2025-11-16 18:08:45 +0300

    fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,7 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        if ($this->app->isLocal()) {
-            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
-        }
+        //
     }
 
     /**
